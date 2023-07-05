4b1e0ebe899e88399d8dfcfa26a2a195d0e7be02
Web3 Developer <wonderfulplayer@outlook.com>
2023-07-05 09:00:01 +0800

    Create environment.staging.ts

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  baseUrl: "https://testserver.empower.eco",
+  clientBaseUrl: "https://test.empower.eco",
+  stripeKey: "pk_test_y6QW8FIwsUF304Ae7IjPMN2v"
+};
