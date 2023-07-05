2645f70ef7203411054ce9870cce9967c7a91faf
Web3 Developer <wonderfulplayer@outlook.com>
2023-07-05 08:59:58 +0800

    Create environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  baseUrl: "https://server.empower.eco",
+  clientBaseUrl: "https://empower.eco",
+  stripeKey: "pk_live_UhYnjsV5HFLzoDhXCKYqRAUH"
+};
